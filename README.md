8e167c4f01283af762aab861232cd3171fff5513
Nafis <nafisahnaf@yahoo.com>
2025-05-30 00:53:36 +0600

    Added base parameter to vite.config.js

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 This app aims to work as a Countdown App Primarily focusing on 
 Ramadan Countdown App:
-# V 0.1
-	This version will use static data to calculate the time remaining for Ramadan to arrive. 
-	-Features:
-    • Dark Themed Countdown with Months, Days, Hours, Minutes, Seconds. 
+
+V 0.1
+	- This version will use static data to calculate the time remaining for Ramadan to arrive. 
+	- Features:
+    • Dark Themed Countdown with Months, Days, Hours, Minutes,      Seconds. 
     • A Simple static webpage that does not require a backend.
     • Uses the inbuilt Date.now() function built in Javascript for fetching client-side date and time. 
 	
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Home = () => {
     const [c, setDate] = useState('');
@@ -6,17 +6,67 @@ const Home = () => {
     const [hoursV, setHoursV] = useState('');
     const [minutesV, setMinutesV] = useState('');
     const [Cols, setCols] = useState(4);
+    
+    //Get Location:
+
+    // const [geoPermissionState, setGeoPermissionState] = useState('');
+
+    // const success = (pos)=>{
+    //     var crd = pos.coords;
+    //     console.log(`Coordinates Lat: ${crd.latitude} Long: ${crd.longitude}, Accuracy: ${crd.accuracy}`);
+    // }
+    // const err = (errMsg)=>{
+    //     console.log(errMsg);    
+    // }
+    // var  options = {
+    //     enableHighAccuracy: true,
+    //     timeout: 5000,
+    //     maximumAge: 0,
+    // }
+    // useEffect(()=>{
+    //     if(navigator.geolocation){
+    //         navigator.permissions.
+    //         query({name: 'geolocation'}).
+    //         then((result)=>{
+    //             console.log(result);
+    //             if(result.state === 'prompt'){
+    //                 navigator.geolocation.getCurrentPosition(success, err, options);    
+    //             }
+    //             else if(result.state === 'granted'){
+    //                 navigator.geolocation.getCurrentPosition(success, err, options);
+    //             }
+    //             else if(result.state === 'denied'){
+    //                 alert("Locaiton Permissions Denied. Please Allow Location Permission in site settings");
+    //             }
+    //             result.onchange = ()=>{setGeoPermissionState(result.state)};
+    //         })
+    //     }
+    //     else{
+    //         console.log("geolocation not supported");
+    //     }
+    // }, [])
+
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(success, err, options);
+    }, [geoPermissionState]);
+
     const d = new Date(2026,1,17,17,55,0,0);
-    setInterval(()=>{
-        setDate(new Date());
-    }, 1000)
-    console.log((d-c))
+
+    //Refreshing every second: 
+    useEffect(()=>{
+        setInterval(()=>{
+            setDate(new Date());
+        }, 1000);
+    }, []);
+
+    
+
     let rem = d-c;
-    const mon =  rem/2592000000;
     const days =  rem/86400000;
     const hours = (days - Math.floor(days))*24;
     const minutes = (hours - Math.floor(hours))*60;
     const seconds = (minutes - Math.floor(minutes))*60;
+    //Display status
     if(days==0){
         setCols(3);
         setDaysV('hidden');
@@ -29,11 +79,10 @@ const Home = () => {
             }
         }
     }
-    // console.log(seconds);
     
     return (
     <div className='h-screen flex flex-col justify-center items-center space-y-12 text-center'>
-        <div className='text-4xl' >Ramadan Countdown</div>
+        <div className='text-4xl'>Ramadan Countdown</div>
         <div className={Cols+' grid grid-cols-1 md:grid-cols-4 space-y-6 md:space-x-6'}>
             {/* <div>
                 <div className='text-6xl' onLoad={(e)=>setDate(d.getMonth())} >{d.getMonth()+1}</div>
